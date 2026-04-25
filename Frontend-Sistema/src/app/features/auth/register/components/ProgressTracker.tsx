dd05fefe33e14a56c5368280ced59015986fb771
ZDevCode04 <u23234027@utp.edu.pe>
2026-04-25 13:12:34 -0500

    feat(shared): implementar ProgressTracker modular

---
+++ Frontend-Sistema/src/app/features/auth/register/components/ProgressTracker.tsx
@@ -0,0 +1,41 @@
+import { CheckCircle2 } from "lucide-react";
+import type { ProgressTrackerProps } from "../../../../core/models/ProgressTrackerProps";
+
+const STEPS_CONFIG = [
+    { id: 1, label: 'Datos' },
+    { id: 2, label: 'Correo' },
+    { id: 3, label: 'DNI' },
+    { id: 4, label: 'Final' }
+];
+
+export default function ProgressTracker({ currentStep }: ProgressTrackerProps) {
+    return (
+        <div className="flex justify-between items-center px-2 max-w-md mx-auto mb-10">
+            {STEPS_CONFIG.map((step) => (
+                <div key={step.id} className="flex flex-col items-center gap-2">
+
+                    <div
+                        className={`h-8 w-8 rounded-full flex items-center justify-center text-xs font-bold transition-all 
+            ${currentStep >= step.id
+                                ? 'bg-teal-500 text-white shadow-md shadow-teal-100'
+                                : 'bg-slate-100 text-slate-400'
+                            }`}
+                    >
+                        {currentStep > step.id ? (
+                            <CheckCircle2 size={16} />
+                        ) : (
+                            step.id
+                        )}
+                    </div>
+
+                    <span
+                        className={`text-[10px] font-bold uppercase tracking-tighter transition-colors
+            ${currentStep >= step.id ? 'text-teal-600' : 'text-slate-400'}`}
+                    >
+                        {step.label}
+                    </span>
+                </div>
+            ))}
+        </div>
+    );
+}
